0a13ec680f2f3147e9b89e9e47ba7ecec109f82a
Jinma Yamashita <jinmayamashita@gmail.com>
2020-01-13 22:20:42 +0900

    fix: incorrect condition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,6 @@ import { Home } from "../containers";
 import { TBD } from "../sandbox/TBD";
 
 // eslint-disable-next-line functional/no-expression-statement
-const index = !isDev() ? Home : TBD;
+const index = isDev() ? Home : TBD;
 
 export default index;
